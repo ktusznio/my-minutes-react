d0e68bf8f6184e315f4c6134e94d37fe088cba3f
Kamil Tusznio <kamil.tusznio@gmail.com>
2016-09-01 14:31:14 -0400

    Navigate to the tasks screen on login.

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -1,26 +1,68 @@
-import * as React from 'react';
 import { RaisedButton } from 'material-ui';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 
-import { signInWithRedirect, facebookAuthProvider } from '../firebase/auth';
+import auth, { signInWithRedirect, facebookAuthProvider } from '../firebase/auth';
+import { IAppState } from '../reducer';
+import { IAuthState } from '../reducers/auth';
 import FacebookIcon from './FacebookIcon';
 import Navigation from './Navigation';
 import { Screen, ScreenContent } from './Screen';
 
-const handleFacebookLoginTap = () => {
-  signInWithRedirect(facebookAuthProvider);
-};
+interface ILoginScreenProps {
+  auth: IAuthState;
+}
 
-const LoginScreen = (props) =>
-  <Screen>
-    <Navigation title="My Minutes" />
-    <ScreenContent>
-      <RaisedButton
-       label="Login with Facebook"
-       icon={<FacebookIcon />}
-       onTouchTap={handleFacebookLoginTap}
-       primary={true}
-     />
-    </ScreenContent>
-  </Screen>
+interface IRouterContext {
+  router: any;
+}
 
-export default LoginScreen;
+const mapStateToProps = (state: IAppState): ILoginScreenProps => ({
+  auth: state.auth,
+})
+
+class LoginScreen extends React.Component<ILoginScreenProps, {}> {
+  static contextTypes = {
+    router: React.PropTypes.func.isRequired,
+  };
+
+  context: IRouterContext;
+
+  handleFacebookLoginTap = () => {
+    signInWithRedirect(facebookAuthProvider);
+  };
+
+  componentWillMount() {
+    // Check for a successful login through the redirect flow and navigate to
+    // the tasks screen.
+    // For the logout flow, avoid the redirect by checking for a logged-in user.
+    if (!this.props.auth.user) {
+      auth.getRedirectResult().then(result => {
+        if (result.user) {
+          browserHistory.replace('/');
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Screen>
+        <Navigation title="My Minutes" />
+        <ScreenContent>
+          <RaisedButton
+           label="Login with Facebook"
+           icon={<FacebookIcon />}
+           onTouchTap={this.handleFacebookLoginTap}
+           primary={true}
+         />
+        </ScreenContent>
+      </Screen>
+    );
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(LoginScreen);
