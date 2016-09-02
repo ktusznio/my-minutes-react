006347e032cede08f33d8cb56a4b04f3403c7ae1
Kamil Tusznio <kamil.tusznio@gmail.com>
2016-09-02 14:36:34 -0400

    Include sw-push via importScripts option to sw-precache plugin.
    And disable service worker’s fetch intercept in development to avoid aggressive caching.

--- sw-precache-webpack-plugin.js
+++ sw-precache-webpack-plugin.js
@@ -2,7 +2,11 @@ var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 module.exports = new SWPrecacheWebpackPlugin({
   cacheId: 'my-minutes',
-  filename: 'sw-precache.js',
+  filename: 'sw-main.js',
+  handleFetch: process.env.NODE_ENV !== 'development',
+  importScripts: [
+    '/sw-push.js',
+  ],
   staticFileGlobs: [
     'public/bundle.js',
     'public/index.html',
