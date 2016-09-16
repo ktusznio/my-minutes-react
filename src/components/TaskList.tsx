ff6ea4a612eaa872be557a8a54c0e66779e3a446
Kamil Tusznio <kamil.tusznio@gmail.com>
2016-09-16 13:39:46 -0400

    Show a message to add a task.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,22 +1,45 @@
 import * as React from 'react';
 import { browserHistory } from 'react-router';
+import NavigationArrowDownward from 'material-ui/svg-icons/navigation/arrow-downward';
 
 import * as m from '../models';
 import TaskListItem from '../components/TaskListItem';
 import { IViewTask } from '../selectors';
+import { Column } from './Flex';
 
 export interface ITaskListProps {
   tasks: IViewTask[];
 }
 
-const TaskList = (props: ITaskListProps) =>
-  <div>
-    {props.tasks.map((task, i) =>
-      <TaskListItem
-        key={i}
-        task={task}
-      />
-    )}
-  </div>
+const TaskList = (props: ITaskListProps) => {
+  if (props.tasks.length === 0) {
+    return <EmptyTaskList />;
+  } else {
+    return (
+      <div>
+        {props.tasks.map((task, i) =>
+          <TaskListItem
+            key={i}
+            task={task}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+const EmptyTaskList = () =>
+  <Column style={style.emptyTaskList}>
+    Add a task to get started. Tap + below.
+  </Column>
+
+const style = {
+  emptyTaskList: {
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+};
 
 export default TaskList;
